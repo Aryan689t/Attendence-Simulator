8bdcd7968adedc396cfe6a9e7dcfa3d5bd10c940
Tathagat Aryan <tathagataryan2006@gmail.com>
2026-04-27 21:51:31 +0530

    Implement local storage for attendance data
    Refactor attendance tracking to use local storage for data persistence. Update event listeners to manage attendance and conducted counts correctly.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let data = JSON.parse(localStorage.getItem("attendanceData")) || [];
 const addbttn=document.getElementById("addbtn");
 const subject=new Set();
    let totalattended=0;
@@ -11,12 +11,21 @@ addbttn.addEventListener("click",()=>{
     const atnd=Number(document.getElementById("attended").value);
     const cndctd=Number(document.getElementById("conducted").value);
 
+   
     if(sub!=""&&(atnd>=0&&cndctd>0)&&(cndctd>atnd)){
-
-         if(subject.has(sub)){
+        
+  if(subject.has(sub)){
         alert("already added");
         return;
         }
+
+        data.push({
+           subject: sub,
+            attended: atnd,
+           conducted: cndctd
+       });
+
+        saveData();
 
         const card=document.createElement("div");
         card.className="card";
@@ -55,6 +64,10 @@ addbttn.addEventListener("click",()=>{
 
         atnpls.addEventListener("click",()=>{
                 attended++;
+                const item = data.find(item => item.subject === sub);
+                item.attended = attended;
+                item.conducted = conducted;
+                 saveData();
                 if(attended>conducted){
                         if(attended > 0){
                           attended--;
@@ -71,6 +84,10 @@ addbttn.addEventListener("click",()=>{
         })    
         atnmins.addEventListener("click",()=>{
                 attended--;
+                const item = data.find(item => item.subject === sub);
+item.attended = attended;
+item.conducted = conducted;
+saveData();
                 atnbox.innerText=attended;
                   totalattended--;
                // allperc(totalattended,totalcounducted);
@@ -80,6 +97,10 @@ addbttn.addEventListener("click",()=>{
         })   
         conpls.addEventListener("click",()=>{
                 conducted++;
+                const item = data.find(item => item.subject === sub);
+item.attended = attended;
+item.conducted = conducted;
+saveData();
                 condbox.innerText=conducted;
                   totalcounducted++;
                 //allperc(totalattended,totalcounducted);
@@ -89,6 +110,10 @@ addbttn.addEventListener("click",()=>{
         }) 
          conmins.addEventListener("click",()=>{
                  conducted--;
+                 const item = data.find(item => item.subject === sub);
+item.attended = attended;
+item.conducted = conducted;
+saveData();
                  if(conducted<attended){
                         alert("study math first");
                         return;
@@ -124,12 +149,15 @@ addbttn.addEventListener("click",()=>{
         delbtn.addEventListener("click",()=>{
                 card.remove();
                 subject.delete(sub);
+                data = data.filter(item => item.subject !== sub);
+                saveData();
                 totalattended -= attended;
                 totalcounducted -= conducted;
                // allperc(totalattended,totalcounducted);
                                 allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
 
         })
+               
 
     }
     else if(cndctd<atnd){
@@ -146,3 +174,136 @@ function allperc(x,y){
         if(y === 0) return 0;
         return Math.round((x/y)*100);
 }
+function showsaved(sub,atnd,cndctd){
+const card=document.createElement("div");
+        card.className="card";
+
+        const subbox=document.createElement("div");
+        subbox.innerText=sub;
+        const atnbox=document.createElement("div");
+        atnbox.innerText=Number(atnd);
+        const condbox=document.createElement("div");
+        condbox.innerText=Number(cndctd);
+       
+        let attended=atnd;
+        let conducted=cndctd;
+        const percbox = document.createElement("div");
+        percbox.innerText = percentage(attended, conducted) + "%";
+
+     
+        totalattended+=attended;
+        totalcounducted+=conducted;
+       // allperc(totalattended,totalcounducted);
+                allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
+
+        
+
+        const atnpls=document.createElement("button");
+                atnpls.innerText="+";
+        const atnmins=document.createElement("button");
+                atnmins.innerText="-";
+        const conpls=document.createElement("button");
+                conpls.innerText="+";
+        const conmins=document.createElement("button");
+                conmins.innerText="-";
+        const delbtn=document.createElement("button");
+                delbtn.innerText="delete";
+
+
+        atnpls.addEventListener("click",()=>{
+                attended++;
+                 const item = data.find(item => item.subject === sub);
+                item.attended = attended;
+                item.conducted = conducted;
+                 saveData();
+                if(attended>conducted){
+                        if(attended > 0){
+                          attended--;
+                        }
+                        alert("seriously???");
+                        return;
+                }
+                totalattended++;
+               // allperc(totalattended,totalcounducted);
+                atnbox.innerText=attended;
+                percbox.innerText=percentage(attended,conducted)+"%";
+                                allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
+
+        })    
+        atnmins.addEventListener("click",()=>{
+                attended--;
+                 const item = data.find(item => item.subject === sub);
+                item.attended = attended;
+                item.conducted = conducted;
+                 saveData();
+                atnbox.innerText=attended;
+                  totalattended--;
+               // allperc(totalattended,totalcounducted);
+                 percbox.innerText=percentage(attended,conducted)+"%";
+                                 allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
+
+        })   
+        conpls.addEventListener("click",()=>{
+                conducted++;
+                 const item = data.find(item => item.subject === sub);
+                item.attended = attended;
+                item.conducted = conducted;
+                 saveData();
+                condbox.innerText=conducted;
+                  totalcounducted++;
+                //allperc(totalattended,totalcounducted);
+                percbox.innerText=percentage(attended,conducted)+"%";
+                                allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
+
+        }) 
+         conmins.addEventListener("click",()=>{
+                 conducted--;
+                  const item = data.find(item => item.subject === sub);
+                item.attended = attended;
+                item.conducted = conducted;
+                 saveData();
+                 if(conducted<attended){
+                        alert("study math first");
+                        return;
+                 }
+                   totalcounducted--;
+                //allperc(totalattended,totalcounducted);
+                condbox.innerText=conducted;
+                percbox.innerText=percentage(attended,conducted)+"%";
+                                allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
+
+        })       
+       
+
+                card.appendChild(subbox);
+                                card.appendChild(atnpls);
+                card.appendChild(atnbox);
+                                card.appendChild(atnmins);
+                                card.appendChild(conpls);
+                card.appendChild(condbox);
+                                card.appendChild(conmins);
+                card.appendChild(percbox);
+                card.appendChild(delbtn);
+
+        const show=document.getElementById("show");
+        show.appendChild(card);   
+
+
+           delbtn.addEventListener("click",()=>{
+                card.remove();
+                subject.delete(sub);
+                data = data.filter(item => item.subject !== sub);
+                saveData();
+                totalattended -= attended;
+                totalcounducted -= conducted;
+               // allperc(totalattended,totalcounducted);
+                                allpercbox.innerText=Number(allperc(totalattended,totalcounducted))+"%";
+subject.add(sub);
+        })  
+}
+function saveData(){
+   localStorage.setItem("attendanceData", JSON.stringify(data));
+}
+data.forEach(item=>{
+   showsaved(item.subject, item.attended, item.conducted);
+});
